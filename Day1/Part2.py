773246eb76aed2b54eaaaf699f4ee64cbf9a5384
Stefan <stevetv90@gmail.com>
2021-02-04 22:12:15 +0100

    Day1 done

--- Day1/Part2.py
+++ Day1/Part2.py
@@ -1,28 +1,49 @@
-coords = open("input.txt","r").read().strip().split(", ")
-rot = 0
-moved = [0,0,0,0]
-hasbeen = set()
-_break = False
+from collections import defaultdict
 
-while True:
-    for c in coords:
-        if c[0] == "R":
-            rot -= 1
-            if rot == -1:
-                rot = 3
-        else:
-            rot = (rot+1)%4
-        moved[rot] += int(c[1:])
-        tmp = str(moved[0]-moved[2])+" "+str(moved[1]-moved[3])
-        print(tmp)
-        if tmp in hasbeen:
-            _break = True
+def walk(hasbeen,moved,rot,new):
+    for i in range(1,new+1):
+        if rot == 0:
+            if hasbeen[str(moved[0]-moved[2]+i)+" "+str(moved[1]-moved[3])] == True:
+                return hasbeen, (moved[0]-moved[2]+i)+(moved[1]-moved[3])
+            hasbeen[str(moved[0]-moved[2]+i)+" "+str(moved[1]-moved[3])] = True
+        if rot == 1:
+            if hasbeen[str(moved[0]-moved[2])+" "+str(moved[1]-moved[3]+i)] == True:
+                return hasbeen, (moved[0]-moved[2])+(moved[1]-moved[3]+i)
+            hasbeen[str(moved[0]-moved[2])+" "+str(moved[1]-moved[3]+i)] = True
+        if rot == 2:
+            if hasbeen[str(moved[0]-moved[2]-i)+" "+str(moved[1]-moved[3])] == True:
+                return hasbeen, (moved[0]-moved[2]-i)+(moved[1]-moved[3])
+            hasbeen[str(moved[0]-moved[2]-i)+" "+str(moved[1]-moved[3])] = True
+        if rot == 3:
+            if hasbeen[str(moved[0]-moved[2])+" "+str(moved[1]-moved[3]-i)] == True:
+                return hasbeen, (moved[0]-moved[2])+(moved[1]-moved[3]-i)
+            hasbeen[str(moved[0]-moved[2])+" "+str(moved[1]-moved[3]-i)] = True
+    return hasbeen, None
+
+def part2():
+    coords = open("input.txt","r").read().strip().split(", ")
+    rot = 0
+    moved = [0,0,0,0]
+    hasbeen = defaultdict(lambda: False)
+    _break = False
+    output = 0
+
+    while True:
+        for c in coords:
+            if c[0] == "R":
+                rot -= 1
+                if rot == -1:
+                    rot = 3
+            else:
+                rot = (rot+1)%4
+            hasbeen, output = walk(hasbeen,moved,rot,int(c[1:]))
+            moved[rot] += int(c[1:])
+            if output != None:
+                _break = True
+                break
+        if _break == True:
             break
-        hasbeen.add(tmp)
-    if _break == True:
-        break
 
-print(moved[0],moved[1],moved[2],moved[3])
-print(abs(moved[0]-moved[2])+abs(moved[1]-moved[3]))
+    print(abs(output))
 
-#275 high
+part2()
